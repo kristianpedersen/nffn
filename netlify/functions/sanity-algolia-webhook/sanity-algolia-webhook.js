6c1431f67cff674f0b6a00e4046d78c419fc9633
Kristian Pedersen <furkrp@furst.no>
2022-10-07 10:42:12 +0200

    Access indexer.default explicitly

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -2,7 +2,7 @@
 const algoliasearch = require("algoliasearch");
 const sanityClient = require("@sanity/client");
 const indexer = require("sanity-algolia");
-
+console.log(indexer.default);
 const algolia = algoliasearch(
   'C26QC41PWH',
   "e23b64dadd4c26f8678c15a2593521fa"
@@ -16,7 +16,9 @@ const sanity = sanityClient({
 });
 
 const handler = async (event) => {
-  const sanityAlgolia = indexer(
+  // `indexer` contains three functions: `default`, `flattenBlocks` og `indexMapProjection`
+  // It seems that we need to access `indexer.default` explicitly when not using JS import syntax
+  const sanityAlgolia = indexer.default(
     {
       post: {
         index: algolia.initIndex('posts'),
