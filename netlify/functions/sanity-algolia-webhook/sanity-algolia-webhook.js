245db2c3cb87c33fc0186c4831af8e72c97685b8
Kristian Pedersen <furkrp@furst.no>
2022-10-07 09:46:35 +0200

    logsanity

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -1,16 +1,23 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const algoliasearch = require("algoliasearch");
 
+const algolia = algoliasearch(
+  'C26QC41PWH',
+  "e23b64dadd4c26f8678c15a2593521fa"
+);
+
+const sanity = sanityClient({
+  projectId: 'sukats6f',
+  dataset: 'test',
+  apiVersion: '2022-09-25',
+  useCdn: false,
+});
+
 const handler = async (event) => {
-  console.log({ event })
   try {
-    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify(event, null, 2),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify(sanity, null, 2),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
