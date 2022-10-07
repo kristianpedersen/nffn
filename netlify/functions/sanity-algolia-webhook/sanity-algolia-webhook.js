7ef41d86bf34bb61f6165250a6e60fe6b6f2ee82
Kristian Pedersen <furkrp@furst.no>
2022-10-07 14:13:40 +0200

    Enklere kode. Returner hardkodet string.

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -2,22 +2,43 @@
 const algoliasearch = require("algoliasearch");
 const sanityClient = require("@sanity/client");
 const indexer = require("sanity-algolia");
+
 const algolia = algoliasearch(
   'C26QC41PWH',
   "e23b64dadd4c26f8678c15a2593521fa"
 );
 
-const sanity = sanityClient({
+const sanityClientInstance = sanityClient({
   projectId: 'sukats6f',
   dataset: 'test',
   apiVersion: '2022-09-25',
   useCdn: false,
 });
 
-const handler = async (event, blabla) => {
-  return { 
-    event,
-    blabla
+const handler = async (event) => {
+  const algoliaIndex = algolia.initIndex("testsoek");
+
+  const sanityAlgolia = indexer.default(
+    {
+      standardArticle: {
+        index: algoliaIndex,
+        projection: `{title}`
+      }
+    },
+
+    document => {
+      return {
+        title: document.heading,
+        body: document.body,
+      };
+    }
+  );
+
+  const test = sanityAlgolia.webhookSync(sanityClientInstance, event.body);
+
+  return {
+    statusCode: 200,
+    body: "Alt gikk bra!"
   };
 }
 
