68065d5c65aafa3c6a69d5fee5a4150bfa53689a
Kristian Pedersen <furkrp@furst.no>
2022-10-07 13:52:52 +0200

    Add correct Sanity name

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -2,7 +2,6 @@
 const algoliasearch = require("algoliasearch");
 const sanityClient = require("@sanity/client");
 const indexer = require("sanity-algolia");
-console.log(indexer.default);
 const algolia = algoliasearch(
   'C26QC41PWH',
   "e23b64dadd4c26f8678c15a2593521fa"
@@ -16,22 +15,22 @@ const sanity = sanityClient({
 });
 
 const handler = async (event) => {
-  // `indexer` contains three functions: `default`, `flattenBlocks` og `indexMapProjection`
+  // `indexer` contains three functions: `default`, `flattenBlocks` and `indexMapProjection`
   // It seems that we need to access `indexer.default` explicitly when not using JS import syntax
   const sanityAlgolia = indexer.default(
     {
-      post: {
-        index: algolia.initIndex('posts'),
+      standardArticle: {
+        index: algolia.initIndex('test_index'),
       },
     },
 
     document => {
       switch (document._type) {
-        case 'standard-article':
+        case 'standardArticle':
           return {
             title: document.title,
-            path: document.slug.current,
-            publishedAt: document.publishedAt,
+            // path: document.slug.current,
+            // publishedAt: document.publishedAt,
             // excerpt: flattenBlocks(document.excerpt),
           };
         default:
@@ -41,7 +40,7 @@ const handler = async (event) => {
   );
 
   return sanityAlgolia
-    .webhookSync(sanity, "oi")
+    .webhookSync(sanity, event.body || "oi")
     .then(() => {
       return {
         statusCode: 200,
