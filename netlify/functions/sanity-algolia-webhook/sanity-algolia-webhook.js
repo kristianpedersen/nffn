2ed6dc43a497dc3b9f6a060daf087e89106ed0d5
Kristian Pedersen <furkrp@furst.no>
2022-10-10 12:24:50 +0200

    Hadde jeg bare lastet opp feil dataformat?

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -5,6 +5,13 @@ const client = algoliasearch(
   "e23b64dadd4c26f8678c15a2593521fa"
 );
 
+const sanityClientInstance = sanityClient({
+  projectId: 'sukats6f',
+  dataset: 'test',
+  apiVersion: '2022-09-25',
+  useCdn: false,
+});
+
 const index = client.initIndex("Sanity-Algolia");
 // const articlesJSON = require("./articles.json");
 
@@ -14,9 +21,54 @@ const index = client.initIndex("Sanity-Algolia");
 //   console.log(objectIDs);
 // });
 
+const mapDocumentTypeToSearchIndex = {
+  post: {
+    index,
+    projection: `{
+          title,
+          "path": slug.current,
+          "body": pt::text(body)
+        }`,
+  },
+
+  article: {
+    index,
+    projection: `{
+          heading,
+          "body": pt::text(body),
+          "authorNames": authors[]->name
+        }`,
+  },
+};
+
+const convertSanityDocumentToAlgoliaRecord = document => {
+  switch (document._type) {
+    case 'post':
+      return Object.assign({}, document, {
+        custom: 'An additional custom field for posts, perhaps?',
+      })
+    case 'article':
+      return {
+        title: document.heading,
+        body: document.body,
+        authorNames: document.authorNames,
+      }
+    default:
+      return document
+  }
+}
+
 const handler = event => {
-  console.log({ event });
-  return JSON.stringify(event);
+  const sanityAlgolia = indexer(mapDocumentTypeToSearchIndex, convertSanityDocumentToAlgoliaRecord);
+
+  return sanityAlgolia
+    .webhookSync(sanityClientInstance, event.body)
+    .then(() => {
+      return {
+        statusCode: 200,
+        body: `Alt gikk bra!`
+      };
+    })
 }
 
 module.exports = { handler }
