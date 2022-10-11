5a0a3e1c0a4ec1b8a90bd1b5fdfd17776e9a2104
Kristian Pedersen <furkrp@furst.no>
2022-10-11 13:09:19 +0200

    Trenger ikke nullsjekk. Logger event for å finne ID

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -12,16 +12,17 @@ const client = algoliasearch(AlgoliaProjectID, AlgoliaApiKey, {
 const index = client.initIndex("Sanity-Algolia");
 
 const handler = async event => {
+  console.log({ event });
   try {
     const { created, deleted, updated } = event.body; // These contain either [null] or [Algolia record ID(s)]
 
-    const savedObjects = await index.saveObjects(created || [], { autoGenerateObjectIDIfNotExist: true });
+    const savedObjects = await index.saveObjects(created, { autoGenerateObjectIDIfNotExist: true });
     console.log({ savedObjects });
 
-    const deletedObjects = await index.deleteObjects(deleted || []);
+    const deletedObjects = await index.deleteObjects(deleted);
     console.log({ deletedObjects });
 
-    const updatedObjects = await index.partialUpdateObjects(updated || [], { createIfNotExists: false });
+    const updatedObjects = await index.partialUpdateObjects(updated, { createIfNotExists: false });
     console.log({ updatedObjects });
 
     return {
