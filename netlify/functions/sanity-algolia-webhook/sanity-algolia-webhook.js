c6bbc87e18fbf0b8685322a69464f3d1e005893d
Kristian Pedersen <furkrp@furst.no>
2022-10-11 13:01:35 +0200

    Kaster litt mer console.log på problemet

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -16,8 +16,13 @@ const handler = async event => {
     const { created, deleted, updated } = event.body; // These contain either [null] or [Algolia record ID(s)]
 
     const savedObjects = await index.saveObjects(created, { autoGenerateObjectIDIfNotExist: true });
+    console.log({ savedObjects });
+
     const deletedObjects = await index.deleteObjects(deleted);
+    console.log({ deletedObjects });
+
     const updatedObjects = await index.partialUpdateObjects(updated, { createIfNotExists: false });
+    console.log({ updatedObjects });
 
     return {
       statusCode: 200,
