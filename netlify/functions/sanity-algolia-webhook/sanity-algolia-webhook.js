b7cd999006a8f8d618e024073746fb0c61898eba
Kristian Pedersen <furkrp@furst.no>
2022-10-12 14:43:54 +0200

    Definitivt samme node-versjon

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -1,9 +1,9 @@
 import { LogLevelEnum } from '@algolia/logger-common';
 import { createConsoleLogger } from '@algolia/logger-console';
 import algoliasearch from 'algoliasearch';
-// import fetch from "node-fetch";
+import fetch from "node-fetch";
 
-const PROD = true;
+const PROD = false;
 
 export const handler = async event => {
   try {
@@ -32,9 +32,9 @@ export const handler = async event => {
     const sanityURL = `https://${sanityProjectID}.api.sanity.io/v2021-06-07/data/query/test?query=*[_id=="${all[0]}"]{content}`;
     console.log({ sanityURL });
 
-    // const document = await fetch(sanityURL);
-    // const data = await document.json();
-    // console.log({ data: JSON.stringify(data) });
+    const document = await fetch(sanityURL);
+    const data = await document.json();
+    console.log({ data: JSON.stringify(data) });
 
     // const createdOrUpdated = await index.saveObjects(updated || created || [], { autoGenerateObjectIDIfNotExist: true });
     // console.log({createdOrUpdated});
