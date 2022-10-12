b2dfdd231c9628f83226cc4a1476af17d935a071
Kristian Pedersen <furkrp@furst.no>
2022-10-12 14:00:34 +0200

    Er det noe commonJS-greier på Netlify?

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -16,22 +16,16 @@ const index = client.initIndex("Sanity-Algolia");
 
 export const handler = async event => {
   try {
-    const {
-      created,
-      deleted,
-      updated,
-      all
-    } = JSON.parse(event.body).ids; // These contain either [null] or an array of Sanity document IDs
+    // These contain either [null] or an array of Sanity document IDs:
+    const { created, deleted, updated, all } = JSON.parse(event.body).ids;
 
+    // If the webhook was triggered, we can (probably?) assume that all[0] contains a valid Sanity document ID.
     const sanityURL = `https://${sanityProjectID}.api.sanity.io/v2021-06-07/data/query/test?query=*[_id=="${all[0]}"]{content}`;
     console.log({ sanityURL });
 
     const document = await fetch(sanityURL);
-    const json = await document.json();
-    console.log({ json });
-
-    const action = [created, deleted, updated].find(ID => Boolean(ID));
-    console.log({ action });
+    const data = await document.json();
+    console.log({ data: JSON.stringify(data) });
 
     // const createdOrUpdated = await index.saveObjects(updated || created || [], { autoGenerateObjectIDIfNotExist: true });
     // console.log({createdOrUpdated});
