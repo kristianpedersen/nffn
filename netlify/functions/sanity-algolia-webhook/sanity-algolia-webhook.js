06411d51135ce87e6959a14e88450cd34f5fcd46
Kristian Pedersen <furkrp@furst.no>
2022-10-13 08:40:28 +0200

    Sletting trenger ikke å innebære fetch-kall

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -3,48 +3,36 @@ import { createConsoleLogger } from '@algolia/logger-console';
 import algoliasearch from 'algoliasearch';
 import fetch from "node-fetch";
 
-const PROD = true;
-
 export const handler = async event => {
+  const AlgoliaProjectID = 'C26QC41PWH';
+  const AlgoliaApiKey = "e23b64dadd4c26f8678c15a2593521fa";
+  const client = algoliasearch(AlgoliaProjectID, AlgoliaApiKey, {
+    logger: createConsoleLogger(LogLevelEnum.Debug)
+  });
+
+  const sanityProjectID = "sukats6f";
+  const index = client.initIndex("Sanity-Algolia");
+
   try {
-    const AlgoliaProjectID = 'C26QC41PWH';
-    const AlgoliaApiKey = "e23b64dadd4c26f8678c15a2593521fa";
-    const client = algoliasearch(AlgoliaProjectID, AlgoliaApiKey, {
-      logger: createConsoleLogger(LogLevelEnum.Debug)
-    });
-
-    const sanityProjectID = "sukats6f";
-    const index = client.initIndex("Sanity-Algolia");
-
     // These contain either [null] or an array of Sanity document IDs:
-    const { created, deleted, updated, all } = PROD
-      ? JSON.parse(event.body).ids
-      : {
-        created: [null],
-        deleted: [null],
-        updated: ["43cec318-21ad-4e49-a6a9-75a8fa1f4ad6"],
-        all: ["43cec318-21ad-4e49-a6a9-75a8fa1f4ad6"]
-      };
-
-    const action = Object.entries({ created, deleted, updated })
-      .filter(([action, [ID]]) => ID !== null)
-    console.log({ action })
-
-    // If the webhook was triggered, we can (probably?) assume that all[0] contains a valid Sanity document ID.
-    const sanityURL = `https://${sanityProjectID}.api.sanity.io/v2021-06-07/data/query/test?query=*[_id=="${all[0]}"]{content}`;
-
-    const document = await fetch(sanityURL);
-    const response = await document.json();
-    const data = response.result[0].content[0];
-    console.log(JSON.stringify(data))
+    const { created, deleted, updated, all } = JSON.parse(event.body).ids;
 
     let obj = "";
+
     if (created || updated) {
-      obj = await index.saveObjects(data, { autoGenerateObjectIDIfNotExist: true });
-      console.log({ obj });
+      // If the webhook was triggered, we can (probably?) assume that all[0] contains a valid Sanity document ID.
+      const sanityURL = `https://${sanityProjectID}.api.sanity.io/v2021-06-07/data/query/test?query=*[_id=="${all[0]}"]{content}`;
+      const document = await fetch(sanityURL);
+      const response = await document.json();
+      const fetchedDataFromSanity = response.result[0].content[0];
+
+      console.log({ data: JSON.stringify(fetchedDataFromSanity) });
+      obj = await index.saveObjects(fetchedDataFromSanity, { autoGenerateObjectIDIfNotExist: true });
+      console.log({ saved: true, obj });
+
     } else if (deleted) {
       obj = index.deleteObjects(data);
-      console.log({ obj });
+      console.log({ deleted: true, obj });
     }
 
     return {
@@ -59,7 +47,3 @@ export const handler = async event => {
     };
   }
 };
-
-PROD
-  ? console.log("Hei fra prod!")
-  : handler()
