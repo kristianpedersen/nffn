ac7d8861bc9aa9b623ebd61114f3cb5dbac2b432
Kristian Pedersen <furkrp@furst.no>
2022-10-24 15:09:49 +0200

    Stringified response

--- netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
+++ netlify/functions/sanity-algolia-webhook/sanity-algolia-webhook.js
@@ -29,7 +29,7 @@ export const handler = async (event) => {
       const sanityURL = `https://${sanityProjectID}.api.sanity.io/v2021-06-07/data/query/test?query=*[_id=="${sanityDocumentID}"]{content}`;
       const response = await fetch(sanityURL);
       const data = await response.json();
-      console.log({ data: data.result }); // Hent tittel-feltet, og husk å endre til "overskrift" når den tiden kommer
+      console.log({ data: JSON.stringify(data) }); // Hent tittel-feltet, og husk å endre til "overskrift" når den tiden kommer
       const contentFromSanityAPI = data?.result[0]?.content;
 
       const content = portableTextToPlainText(contentFromSanityAPI);
